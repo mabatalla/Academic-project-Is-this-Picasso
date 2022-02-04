16060c819374a29bb882ac96e4d74ee3919ed27c
mabatalla <miguelangel@batalla.pro>
2022-02-04 14:17:16 +0100

    color_quant funcs

--- utils.py
+++ utils.py
@@ -1,38 +1,26 @@
+# IMPORTS
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+
+from collections import Counter
 from sklearn.cluster import KMeans
 
 
-# To import image in RGB mode
-def get_image_rgb(image_path):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # image = cv2.resize(image, (1200, 600))
-    return image
-
-# To import image in greyscale mode
-def get_image_gre(image_path):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # image = cv2.resize(image, (1200, 600))
-    return image
-
-# Transform RGB index to HEX index
-def rgb2hex(color):
-    return f'#{int(color[0]):02x}{int(color[1]):02x}{int(color[2]):02x}'
-
-def get_colors(image, num_of_colors, show_chart=True):
-    # Resize the image to reduce processing time
-    image_res = cv2.resize(image,
-                           (600, 400),
-                           interpolation=cv2.INTER_AREA)
+# FUNCS
+# To extract colors from image
+def color_quant(image, bins, num_of_colors=10, show_chart=True):
+    '''
+    This function applies a color quantization based on cv2.kmeans function
+    as described in the OpenCV docs to reduce the colors present in an image.
+    '''
     
     # Reshape of the image to get np.array 1D (KMeans requirement)
-    image_res = image_res.reshape(image_res.shape[0]*image_res.shape[1], 3)
+    image = image.reshape(image.shape[0]*image.shape[1], 3)
     
     # Use of KMeans to generate num_of_colors of clusters
     model_clf = KMeans(n_clusters=num_of_colors)
-    labels = model_clf.fit_predict(image_res)
+    labels = model_clf.fit_predict(image)
     
     counts = Counter(labels)
     
@@ -41,12 +29,18 @@ def get_colors(image, num_of_colors, show_chart=True):
     
     colors = model_clf.cluster_centers_
     
-    # Get ordered colors iterating through the keys
+    # Transform color indexes to the reduced palette
+    for color in colors:
+            color[0] = process_color(color[0], bins)
+            color[1] = process_color(color[1], bins)
+            color[2] = process_color(color[2], bins)
+    
+    # Get ordered colors and presence iterating through the keys
     ordered_colors = [colors[i] for i in counts.keys()]
     rgb_colors = [ordered_colors[i] for i in counts.keys()]
     
     # Transform RGB index to HEX index
-    hex_colors = [rgb2hex(ordered_colors[i]) for i in counts.keys()]
+    hex_colors = [rgb_to_hex(ordered_colors[i]).upper() for i in counts.keys()]
 
     if (show_chart):
         plt.figure(figsize=(10, 10))
@@ -54,4 +48,91 @@ def get_colors(image, num_of_colors, show_chart=True):
                 labels=hex_colors,
                 colors=hex_colors)
     
-    return rgb_colors
+    return [hex_col.lstrip('#') for hex_col in hex_colors] #hex_colors #rgb_colors
+
+# Transform HEX index to RGB index
+def hex_to_rgb(color):
+    c = color.lstrip('#')
+    for i in (0, 2, 4): color = tuple(int(c[i: i+2], 16))
+    
+    return color
+
+# To determine the filling_ratio
+def fill_ratio(image):
+    w = 0
+    non_w = 0
+
+    for h in range(image.shape[0]):
+        for w in range(image.shape[1]):
+            if (image[h][w] == 255).all():
+                w+=1
+            else:
+                non_w+=1
+        
+    filling_ratio = (non_w*100)/(image.shape[0]*image.shape[1])
+    
+    return filling_ratio
+
+# To import image in RGB mode
+def get_img_rgb(image_path):
+    '''
+    By default, OpenCV reads image in the sequence Blue Green Red (BGR).
+    Thus, to view the actual image we need to convert the rendering
+    to Red Green Blue (RGB).
+    '''
+    
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    return image
+
+# To resize keeping ratio
+def resize_img(image, height):
+    ratio = image.shape[0]/image.shape[1]
+    width = int(height / ratio)
+     
+    return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+
+# Transform RGB index to HEX index
+def rgb_to_hex(color):
+    
+    return f'#{int(color[0]):02x}{int(color[1]):02x}{int(color[2]):02x}'
+
+# To map a rgb channel color value to a reduced palette
+def process_color(channel_value, bins):
+    '''
+    This function is used to map the values of RGB in a pixel that
+    go from 0 to 255 to a simple version with X values that lead to
+    a palette of Y colors (Xx Xx Xx = Y).
+    Eg. X = 4, then y = 4x4x4 = 64 colors
+    '''
+    
+    if channel_value >= 255: processed_value = 255
+    else:
+        preprocessed_value = np.floor((channel_value*bins)/255)
+        processed_value = abs(int(preprocessed_value*(255/(bins-1))))
+    
+    return processed_value
+
+# To map all pixels of an image to a reduced palette
+def reduce_col_palette(image, bins):
+    '''
+    This function iterate through every pixel of an image to map
+    each rgb channel color value to a reduced palette.
+    '''
+    
+    # Capture image dimensions
+    img = image.flatten()
+       
+    # Iterate the array to transform the value of the pixels
+    for px in range(len(img)):
+        if img[px] == 255: img[px] = 255
+        else: img[px] = process_color(img[px], bins)
+    
+    # Restore image shape
+    img = np.reshape(img, image.shape)
+        
+    # Inform user   
+    print(f'Palette reduced to {bins**3} colors.')
+            
+    return img
